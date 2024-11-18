dc02d57538d5f3ebe8179071192b72d800c6d566
Lizzy-2025 <159677331+Lizzy-2025@users.noreply.github.com>
2024-11-18 02:13:20 +0000

    Log In form and style

---
+++ React-Vite/NatureConnectVite/src/components/LogInForm.jsx
@@ -0,0 +1,12 @@
+import classes from "./LogInForm.module.css";
+
+function LogInForm() {
+  return (
+    <div className={classes.logInDiv}>
+      <h1>Log In</h1>
+      <h3>Not a member yet? Sign Up</h3>
+    </div>
+  );
+}
+
+export default LogInForm;
